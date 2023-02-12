e46c509e97f857e1db0877b102e8b10940a59dc8
Numahata-Saori <numahata.saori@gmail.com>
2023-02-12 09:55:38 +0900

    contact-thanks add

--- page-contact-thanks.php
+++ page-contact-thanks.php
@@ -1,19 +1,16 @@
 <?php get_header(); ?>
 
+<div class="contact-thanks wow animate__animated wow animate__animated">
 <?php if ( have_posts() ) : ?>
   <?php while ( have_posts() ) : the_post(); ?>
 
   <?php
-
-// 記事の文章
-the_content();
-
-?>
+  // 記事の文章
+  the_content();
+  ?>
 
   <?php endwhile; ?>
 <?php endif; ?>
-
+</div>
 
 <?php get_footer(); ?>
-
-<!-- https://cotodama.co/contact-form-7_css/ -->
